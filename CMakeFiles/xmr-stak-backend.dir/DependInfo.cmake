a4a63b7daf3b43d8b39b097c2c695b8b6e82763a
mustcd <applemlgb@gmail.com>
2018-06-12 19:15:12 +0800

    1

---
+++ CMakeFiles/xmr-stak-backend.dir/DependInfo.cmake
@@ -0,0 +1,47 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/root/data/xmr-stak/xmrstak/backend/backendConnector.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/backendConnector.cpp.o"
+  "/root/data/xmr-stak/xmrstak/backend/cpu/crypto/cryptonight_common.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/crypto/cryptonight_common.cpp.o"
+  "/root/data/xmr-stak/xmrstak/backend/cpu/hwlocMemory.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/hwlocMemory.cpp.o"
+  "/root/data/xmr-stak/xmrstak/backend/cpu/jconf.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/jconf.cpp.o"
+  "/root/data/xmr-stak/xmrstak/backend/cpu/minethd.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/minethd.cpp.o"
+  "/root/data/xmr-stak/xmrstak/backend/globalStates.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/globalStates.cpp.o"
+  "/root/data/xmr-stak/xmrstak/http/httpd.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/http/httpd.cpp.o"
+  "/root/data/xmr-stak/xmrstak/http/webdesign.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/http/webdesign.cpp.o"
+  "/root/data/xmr-stak/xmrstak/jconf.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/jconf.cpp.o"
+  "/root/data/xmr-stak/xmrstak/misc/console.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/console.cpp.o"
+  "/root/data/xmr-stak/xmrstak/misc/executor.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/executor.cpp.o"
+  "/root/data/xmr-stak/xmrstak/misc/telemetry.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/telemetry.cpp.o"
+  "/root/data/xmr-stak/xmrstak/misc/uac.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/uac.cpp.o"
+  "/root/data/xmr-stak/xmrstak/misc/utility.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/utility.cpp.o"
+  "/root/data/xmr-stak/xmrstak/net/jpsock.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/net/jpsock.cpp.o"
+  "/root/data/xmr-stak/xmrstak/net/socket.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/net/socket.cpp.o"
+  "/root/data/xmr-stak/xmrstak/version.cpp" "/root/data/xmr-stak/CMakeFiles/xmr-stak-backend.dir/xmrstak/version.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BACKEND_TYPE=cpu"
+  "CONF_NO_CUDA"
+  "CONF_NO_OPENCL"
+  "GIT_BRANCH=master"
+  "GIT_COMMIT_HASH=b3f79de"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/root/data/xmr-stak/CMakeFiles/xmr-stak-c.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/xmr-stak-c.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/root/data/xmr-stak/xmrstak/backend/cpu/crypto/c_blake256.c" "/root/data/xmr-stak/CMakeFiles/xmr-stak-c.dir/xmrstak/backend/cpu/crypto/c_blake256.c.o"
+  "/root/data/xmr-stak/xmrstak/backend/cpu/crypto/c_groestl.c" "/root/data/xmr-stak/CMakeFiles/xmr-stak-c.dir/xmrstak/backend/cpu/crypto/c_groestl.c.o"
+  "/root/data/xmr-stak/xmrstak/backend/cpu/crypto/c_jh.c" "/root/data/xmr-stak/CMakeFiles/xmr-stak-c.dir/xmrstak/backend/cpu/crypto/c_jh.c.o"
+  "/root/data/xmr-stak/xmrstak/backend/cpu/crypto/c_keccak.c" "/root/data/xmr-stak/CMakeFiles/xmr-stak-c.dir/xmrstak/backend/cpu/crypto/c_keccak.c.o"
+  "/root/data/xmr-stak/xmrstak/backend/cpu/crypto/c_skein.c" "/root/data/xmr-stak/CMakeFiles/xmr-stak-c.dir/xmrstak/backend/cpu/crypto/c_skein.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "BACKEND_TYPE=cpu"
+  "CONF_NO_CUDA"
+  "CONF_NO_OPENCL"
+  "GIT_BRANCH=master"
+  "GIT_COMMIT_HASH=b3f79de"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
